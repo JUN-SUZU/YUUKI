93ebb3147c7f794b96dffec391573ba904a0416e
JUN-SUZU <jun2006.master@gmail.com>
2024-06-05 06:53:18 +0900

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ActionRowBuilder, ActivityType, Client, Collection, EmbedBuilder, Events, GatewayIntentBits, PermissionsBitField } = require('discord.js');
-
+const config = require('./config.json');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -18,4 +18,40 @@ client.once('ready', async () => {
 });
 
 client.on(Events.MessageCreate, async (message) => {
+    spamProtection(message);
 });
+
+function spamProtection(message) {
+    const { author, guild, member } = message;
+    if (author.bot || !guild || !member) return;
+    const { roles } = member;
+    const { cache } = roles;
+    const mutedRole = cache.find(role => role.name === 'Muted');
+    if (mutedRole) {
+        message.delete();
+        return;
+    }
+    const { content } = message;
+    const { spam } = client;
+    if (!spam.has(author.id)) {
+        spam.set(author.id, new Collection());
+    }
+    const timestamps = spam.get(author.id);
+    const now = Date.now();
+    timestamps.set(now, content);
+    setTimeout(() => {
+        timestamps.delete(now);
+        if (!timestamps.size) {
+            spam.delete(author.id);
+        }
+    }, 5000);
+    if (timestamps.size > 5) {
+        message.delete();
+        const { channel } = message;
+        const member = guild.members.cache.get(author.id);
+        member.roles.add(mutedRole);
+        channel.send(`${author}, you have been muted for spamming!`);
+    }
+}
+
+client.login(config.token);
